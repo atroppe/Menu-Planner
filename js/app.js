514473e28e8676974bdfd67d79e8bba5809bbe8f
atroppe <amytroppe@Amys-MacBook-Pro.local>
2014-08-18 16:45:30 -0500

    update 1

--- js/app.js
+++ js/app.js
@@ -1,19 +1,42 @@
 window.onload = app;
 
-function app(){
+function app() {
 
-	var Recipe = Parse.Object.extend("Recipe", {
+    var Recipe = Parse.Object.extend("Recipe", {
 
-	});
+    });
 
-	var RecipeList = Parse.Collection.extend({
-		model: Recipe
-	});
+    var RecipeList = Parse.Collection.extend({
+        model: Recipe
+    });
 
-	Parse.initialize("KtYn68P4rZSjDiYTbbopjSR8isFcpIPUw5gvT41W", "tKtv0jy9BB7BdzBSGUJTFIslsCDX9JcrzAScU0Xk");
+    Parse.initialize("KtYn68P4rZSjDiYTbbopjSR8isFcpIPUw5gvT41W", "tKtv0jy9BB7BdzBSGUJTFIslsCDX9JcrzAScU0Xk");
 
-	var myRecipes = new RecipeList();
-	myRecipes.fetch().then(function(){
-		console.log(myRecipes.toJSON());
-	})
+    var myRecipes = new RecipeList();
+    var recipeData = [];
+
+    myRecipes.fetch().then(function() {
+        var matching = myRecipes.filter(function(r) {
+            return r.attributes.foodCategory === window.location.hash.slice(1);
+        });
+
+        console.log(matching)
+
+        if (matching.length) {
+            for (var i = 0; i < matching.length; i++) {
+                drawRecipe(matching[i]);
+            }
+        }
+    });
+
+    function drawRecipe(recipe) {
+        var myTemplateHTML = $('#card-template')[0].textContent;
+        console.log($('#card-template')[0]);
+        var myTemplatingFn = _.template(myTemplateHTML);
+        var resultingHTML = myTemplatingFn(recipe);
+        console.log(recipe)
+        $('#pageContent').append(resultingHTML);
+        return;
+
+    }
 }
